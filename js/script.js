a0b151627a6d73c132a0c42d7770502894a64796
Markus Szokoll <markus.szokoll@gmx.at>
2019-02-16 15:18:36 +0100

    working & styled

--- js/script.js
+++ js/script.js
@@ -12,24 +12,24 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var ClassLocation = /** @class */ (function () {
-    function ClassLocation(name, city, zip_code, adress, image, created_date, created_time) {
+    function ClassLocation(name, city, zip_code, address, image, created_date, created_time) {
         this.name = name;
         this.city = city;
         this.zip_code = zip_code;
-        this.adress = adress;
+        this.address = address;
         this.image = image;
         this.created_date = created_date;
         this.created_time = created_time;
     }
     ClassLocation.prototype.render = function () {
-        return this.name + "<br/>" + this.adress + ", " + this.zip_code + " " + this.city + "<br/><img width='200px' src='img/" + this.image + "'></img>" + "<br/>" + this.created_date + " " + this.created_time;
+        return "<h4><b>" + this.name + "</b></h4>" + "<br/><b>Address:</b> " + this.address + ", " + this.zip_code + " " + this.city + "<br/><img src='img/" + this.image + "'></img>" + "<br/><b>Entry Created:</b> " + this.created_date + " " + this.created_time;
     };
     return ClassLocation;
 }());
 var ClassPlace = /** @class */ (function (_super) {
     __extends(ClassPlace, _super);
-    function ClassPlace(name, city, zip_code, adress, image, created_date, created_time) {
-        return _super.call(this, name, city, zip_code, adress, image, created_date, created_time) || this;
+    function ClassPlace(name, city, zip_code, address, image, created_date, created_time) {
+        return _super.call(this, name, city, zip_code, address, image, created_date, created_time) || this;
     }
     ClassPlace.prototype.render = function () {
         return _super.prototype.render.call(this);
@@ -38,30 +38,30 @@ var ClassPlace = /** @class */ (function (_super) {
 }(ClassLocation));
 var ClassRestaurant = /** @class */ (function (_super) {
     __extends(ClassRestaurant, _super);
-    function ClassRestaurant(name, city, zip_code, adress, image, created_date, created_time, telephone, type, web_adress) {
-        var _this = _super.call(this, name, city, zip_code, adress, image, created_date, created_time) || this;
+    function ClassRestaurant(name, city, zip_code, address, image, created_date, created_time, telephone, type, web_address) {
+        var _this = _super.call(this, name, city, zip_code, address, image, created_date, created_time) || this;
         _this.telephone = telephone;
         _this.type = type;
-        _this.web_adress = web_adress;
+        _this.web_address = web_address;
         return _this;
     }
     ClassRestaurant.prototype.render = function () {
-        return _super.prototype.render.call(this) + "<br/>" + this.telephone + "<br/>" + this.type + "<br/>" + this.web_adress;
+        return _super.prototype.render.call(this) + "<br/><br/><b>Phone-Number:</b> " + this.telephone + "<br/><b>Restaurant-Type:</b> " + this.type + "<br/><b>Homepage:</b> <a href='" + this.web_address + "' target='_blank'>" + this.web_address + "</a>";
     };
     return ClassRestaurant;
 }(ClassLocation));
 var ClassEvent = /** @class */ (function (_super) {
     __extends(ClassEvent, _super);
-    function ClassEvent(name, city, zip_code, adress, image, created_date, created_time, event_date, event_time, ticket_price_euro, web_adress) {
-        var _this = _super.call(this, name, city, zip_code, adress, image, created_date, created_time) || this;
+    function ClassEvent(name, city, zip_code, address, image, created_date, created_time, event_date, event_time, ticket_price_euro, web_address) {
+        var _this = _super.call(this, name, city, zip_code, address, image, created_date, created_time) || this;
         _this.event_date = event_date;
         _this.event_time = event_time;
         _this.ticket_price_euro = ticket_price_euro;
-        _this.web_adress = web_adress;
+        _this.web_address = web_address;
         return _this;
     }
     ClassEvent.prototype.render = function () {
-        return _super.prototype.render.call(this) + "<br/>" + this.event_date + "<br/>" + this.event_time + "<br/>" + this.ticket_price_euro + "<br/>" + this.web_adress;
+        return _super.prototype.render.call(this) + "<br/><br/><b>Event-Date:</b> " + this.event_date + "<br/><b>Event-Time:</b> " + this.event_time + "<br/><b>Ticket-Price:</b> € " + this.ticket_price_euro.toFixed(2) + "<br/><b>Homepage:</b> <a href='" + this.web_address + "' target='_blank'>" + this.web_address + "</a>";
     };
     return ClassEvent;
 }(ClassLocation));
@@ -95,18 +95,23 @@ function createContent() {
     if (sortType) {
         LocationArray.reverse();
     }
-    document.getElementById("locations").innerHTML = "";
+    var newContent = "<div class='container'>";
     for (var index in LocationArray) {
-        document.getElementById("locations").innerHTML += locationData[LocationArray[index][0]].render() + "<br/>" + "<hr>" + "<br/>";
+        newContent += "<div class='col-lg-3 col-md-6 col-sm-12 col-xs-12 bootheight'>";
+        newContent += locationData[LocationArray[index][0]].render() + "<br/>" + "<hr>" + "<br/>";
+        newContent += "</div>";
     }
+    newContent += "</div>";
+    document.getElementById("locations").innerHTML = newContent;
 }
 var sortType = false;
 var locationData = new Array();
 locationData[0] = new ClassLocation("St. Charles Church", "Wien", "1010", "Karlsplatz 1", "karlsplatz.jpg", "02.01.1999", "08:00");
-locationData[1] = new ClassPlace("Zoo Vienna", "Wien", "1130", "Maxingstraße 13b", "zoo.jpg", "02.01.1999", "09:00");
-locationData[2] = new ClassRestaurant("Lemon Leaf Thai Restaurant", "Wien", "1050", "Kettenbrückengasse 19", "lemonleaf.png", "02.01.1999", "10:00", "+43(1)5812308", "Thai", "http://www.lemonleaf.at/");
+locationData[1] = new ClassPlace("Zoo Vienna", "Wien", "1130", "Maxingstraße 13b", "zoo.jpg", "02.01.1995", "09:00");
+locationData[2] = new ClassRestaurant("Lemon Leaf Thai Restaurant", "Wien", "1050", "Kettenbrückengasse 19", "lemonleaf.png", "02.01.1993", "10:00", "+43(1)5812308", "Thai", "http://www.lemonleaf.at/");
 locationData[3] = new ClassRestaurant("SIXTA", "Wien", "1050", "Schönbrunner Straße 21", "sixta.png", "02.01.1999", "11:00", "+43 1 58 528 56 | +43 1 58 528 56", "Standard", "http://www.sixta-restaurant.at/");
-locationData[4] = new ClassEvent("Kris Kristofferson", "Wien", "1150 ", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "kriskristofferson.jpg", "02.01.1999", "12:01", "15.11.2019", "20:00", 58.5, "http://kriskristofferson.com/");
-locationData[5] = new ClassEvent("Lenny Kravitz", "Wien", "1150 ", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "lenny_kravitz.jpg", "02.01.1999", "11:11", "09.12.2019", "19:30", 47.8, "http://www.lennykravitz.com/");
+locationData[4] = new ClassRestaurant("VEGETASIA", "Wien", "1030", "Ungargasse 57", "vegetasia.jpg", "02.05.1999", "15:00", "+43 1 71 38 332", "Vegetarian", "https://vegetasia.at/");
+locationData[5] = new ClassEvent("Kris Kristofferson", "Wien", "1150 ", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "kriskristofferson.jpg", "02.01.1990", "12:01", "15.11.2019", "20:00", 58.5, "http://kriskristofferson.com/");
+locationData[6] = new ClassEvent("Lenny Kravitz", "Wien", "1150 ", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "lenny_kravitz.jpg", "02.01.1992", "11:11", "09.12.2019", "19:30", 47.8, "http://www.lennykravitz.com/");
 document.getElementById("sortType").addEventListener("click", function () { sort("type"); }, false);
 createContent();
--- js/script.ts
+++ js/script.ts
@@ -2,30 +2,32 @@ class ClassLocation{
 	name: string;
    	city: string;
    	zip_code: string;
-	adress: string;
+	address: string;
 	image: string;
 	created_date: string;
 	created_time: string;
 
-	constructor(name, city, zip_code, adress, image, created_date, created_time){
+	constructor(name, city, zip_code, address, image, created_date, created_time){
 		this.name = name;
 		this.city =  city;
 		this.zip_code = zip_code;
-		this.adress = adress;
+		this.address = address;
 		this.image = image;
 		this.created_date = created_date;
 		this.created_time = created_time;
 	}
 
 	render(){
-			return this.name +"<br/>"+  this.adress + ", " + this.zip_code +" "+ this.city  +"<br/><img width='200px' src='img/"+this.image+"'></img>" + "<br/>"+this.created_date + " " + this.created_time;
+			return "<h4><b>" + this.name + "</b></h4>" +"<br/><b>Address:</b> "+  this.address + ", " + this.zip_code +" "+ this.city + "<br/><img src='img/"+this.image+"'></img>" + "<br/><b>Entry Created:</b> "+this.created_date + " " + this.created_time;
 		}
+		
+
 }
 
 class ClassPlace extends ClassLocation{
 	
-	constructor(name, city, zip_code, adress, image, created_date, created_time){
-		super(name, city, zip_code, adress, image, created_date, created_time);
+	constructor(name, city, zip_code, address, image, created_date, created_time){
+		super(name, city, zip_code, address, image, created_date, created_time);
 	}
 	render(){
 		return super.render();
@@ -35,16 +37,16 @@ class ClassPlace extends ClassLocation{
 class ClassRestaurant extends ClassLocation{
 	telephone: string;
 	type: string;
-	web_adress: string;
+	web_address: string;
 
-	constructor(name, city, zip_code, adress, image, created_date, created_time, telephone, type, web_adress){
-		super(name, city, zip_code, adress, image, created_date, created_time);
+	constructor(name, city, zip_code, address, image, created_date, created_time, telephone, type, web_address){
+		super(name, city, zip_code, address, image, created_date, created_time);
 		this.telephone = telephone;
 		this.type = type;
-		this.web_adress = web_adress;
+		this.web_address = web_address;
 	}
 	render(){
-		return super.render() +"<br/>"+ this.telephone +"<br/>"+ this.type +"<br/>"+ this.web_adress;
+		return super.render() +"<br/><br/><b>Phone-Number:</b> "+ this.telephone +"<br/><b>Restaurant-Type:</b> "+ this.type +"<br/><b>Homepage:</b> <a href='"+this.web_address+"' target='_blank'>"+ this.web_address + "</a>";
 	}
 }
 
@@ -53,17 +55,17 @@ class ClassEvent extends ClassLocation{
 	event_date: string;
 	event_time: string;
 	ticket_price_euro: number;
-	web_adress: string;
+	web_address: string;
 
-	constructor(name, city, zip_code, adress, image, created_date, created_time, event_date, event_time, ticket_price_euro, web_adress){
-		super(name, city, zip_code, adress, image, created_date, created_time);
+	constructor(name, city, zip_code, address, image, created_date, created_time, event_date, event_time, ticket_price_euro, web_address){
+		super(name, city, zip_code, address, image, created_date, created_time);
 		this.event_date = event_date;
 		this.event_time = event_time;
 		this.ticket_price_euro = ticket_price_euro;
-		this.web_adress = web_adress;
+		this.web_address = web_address;
 	}
 		render(){
-		return super.render() +"<br/>"+ this.event_date +"<br/>"+ this.event_time +"<br/>"+ this.ticket_price_euro +"<br/>"+ this.web_adress;
+		return super.render() +"<br/><br/><b>Event-Date:</b> "+ this.event_date +"<br/><b>Event-Time:</b> "+ this.event_time +"<br/><b>Ticket-Price:</b> € "+ this.ticket_price_euro.toFixed(2) +"<br/><b>Homepage:</b> <a href='"+this.web_address+"' target='_blank'>"+ this.web_address + "</a>";
 	}
 }
 
@@ -103,11 +105,15 @@ function createContent(){
 	});
    	if(sortType){LocationArray.reverse()}
 
-   	document.getElementById("locations").innerHTML = "";
+   	var newContent = "<div class='container'>";
 
 	for (let index in LocationArray) {
-		document.getElementById("locations").innerHTML += locationData[LocationArray[index][0]].render()+"<br/>"+"<hr>"+"<br/>";
+		newContent += "<div class='col-lg-3 col-md-6 col-sm-12 col-xs-12 bootheight'>";
+		newContent += locationData[LocationArray[index][0]].render()+"<br/>"+"<hr>"+"<br/>";
+		newContent += "</div>";
 	}
+	newContent +=  "</div>";
+	document.getElementById("locations").innerHTML = newContent;
 }
 
 
@@ -116,11 +122,12 @@ var sortType = false;
 var locationData = new Array();
 
 locationData[0] = new ClassLocation("St. Charles Church","Wien", "1010", "Karlsplatz 1", "karlsplatz.jpg", "02.01.1999","08:00");
-locationData[1] = new ClassPlace("Zoo Vienna","Wien", "1130", "Maxingstraße 13b", "zoo.jpg", "02.01.1999", "09:00");
-locationData[2] = new ClassRestaurant("Lemon Leaf Thai Restaurant","Wien", "1050", "Kettenbrückengasse 19", "lemonleaf.png", "02.01.1999","10:00", "+43(1)5812308","Thai","http://www.lemonleaf.at/");
+locationData[1] = new ClassPlace("Zoo Vienna","Wien", "1130", "Maxingstraße 13b", "zoo.jpg", "02.01.1995", "09:00");
+locationData[2] = new ClassRestaurant("Lemon Leaf Thai Restaurant","Wien", "1050", "Kettenbrückengasse 19", "lemonleaf.png", "02.01.1993","10:00", "+43(1)5812308","Thai","http://www.lemonleaf.at/");
 locationData[3] = new ClassRestaurant("SIXTA","Wien", "1050", "Schönbrunner Straße 21", "sixta.png", "02.01.1999","11:00","+43 1 58 528 56 | +43 1 58 528 56","Standard","http://www.sixta-restaurant.at/");
-locationData[4] = new ClassEvent("Kris Kristofferson","Wien", "1150 ", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "kriskristofferson.jpg", "02.01.1999","12:01","15.11.2019","20:00",58.5,"http://kriskristofferson.com/");
-locationData[5] = new ClassEvent("Lenny Kravitz","Wien", "1150 ", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "lenny_kravitz.jpg", "02.01.1999","11:11","09.12.2019","19:30",47.8,"http://www.lennykravitz.com/");
+locationData[4] = new ClassRestaurant("VEGETASIA","Wien", "1030", "Ungargasse 57", "vegetasia.jpg", "02.05.1999","15:00","+43 1 71 38 332","Vegetarian","https://vegetasia.at/");
+locationData[5] = new ClassEvent("Kris Kristofferson","Wien", "1150 ", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "kriskristofferson.jpg", "02.01.1990","12:01","15.11.2019","20:00",58.5,"http://kriskristofferson.com/");
+locationData[6] = new ClassEvent("Lenny Kravitz","Wien", "1150 ", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "lenny_kravitz.jpg", "02.01.1992","11:11","09.12.2019","19:30",47.8,"http://www.lennykravitz.com/");
 
 document.getElementById("sortType").addEventListener("click", function(){sort("type");},false);
 
